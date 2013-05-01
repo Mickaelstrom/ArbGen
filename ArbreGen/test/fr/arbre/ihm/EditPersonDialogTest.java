ce7bb1057b4843b03d73f78dde5de8035d65727a
Mickael <mickaelgoncalves32@gmail.com>
2013-05-01 17:43:02 +0200

    Améliorations

--- ArbreGen/test/fr/arbre/ihm/EditPersonDialogTest.java
+++ ArbreGen/test/fr/arbre/ihm/EditPersonDialogTest.java
@@ -9,31 +9,44 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
+import fr.arbre.ihm.EditPersonDialog;
+import fr.arbre.model.Gender;
 import fr.arbre.model.SimplePerson;
 
+/**
+ * Test de la fenêtre EditPersonDialog
+ * 
+ * -> Changement du titre de la fenêtre
+ * -> Test création d'une personne
+ * -> Test édition d'une personne
+ */
 public class EditPersonDialogTest {
+
+	static SimplePerson p1, p2;
 
 	public static void main(String[] args) {
 		final JFrame frame = new JFrame("EditPersonDialog Test");
 		JPanel panel = new JPanel();
 		final JButton b1 = new JButton("Créer personne");
 		final JButton b2 = new JButton("Editer personne");
+		p2 = new SimplePerson("nom", "prenom", "12/12/2012", "", Gender.FEMALE,
+				0, 0, null);
 
 		b1.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				SimplePerson value = EditPersonDialog.showDialog(frame, b1,
-						b1.getText());
-				System.out.println("b1" + value.toString());
+				p1 = EditPersonDialog.showDialog(frame);
+				System.out.println("id1= " + p1.getId());
+				System.out.println("id2= " + p2.getId() + " " + p2.toString());
 			}
 		});
 
 		b2.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				SimplePerson value = EditPersonDialog.showDialog(frame, b2,
-						b2.getText());
-				System.out.println("b2" + value.toString());
+				p2 = EditPersonDialog.showDialog(frame, p2);
+				System.out.println("id1= " + p1.getId());
+				System.out.println("id2= " + p2.getId() + " " + p2.toString());
 			}
 		});
 
