02d7a63a3dea2974f586ebea7fec59873bbd4109
Mickaelstrom <mickaelgoncalves32@gmail.com>
2013-05-03 14:38:45 +0200

    Ajout mineur de gestion d'icones

--- ArbreGen/src/fr/arbre/ihm/EditPersonDialog.java
+++ ArbreGen/src/fr/arbre/ihm/EditPersonDialog.java
@@ -8,6 +8,7 @@ import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
@@ -62,12 +63,16 @@ public class EditPersonDialog extends JDialog implements ActionListener {
 
 	private EditPersonDialog(Frame frame, String title, SimplePerson person) {
 		super(frame, title, true);
+		this.setIconImage(new ImageIcon("resources/Icons/personne-16.png").getImage());
 		lambda = person;
+
+		/* PARTIE GAUCHE ----------------------------------------------------------- */
 
 		// TODO mettre le contenu de la fenetre ici
 
 		/* PARTIE DROITE ----------------------------------------------------------- */
-		setLayout(new GridBagLayout());
+		setLayout(new GridBagLayout()); // TODO à adapter quand classe complété
+		
 		GridBagConstraints c = new GridBagConstraints();
 		c.gridy = 0;
 		c.fill = GridBagConstraints.HORIZONTAL;
@@ -79,20 +84,20 @@ public class EditPersonDialog extends JDialog implements ActionListener {
 		c.gridwidth = 3;
 		add(fatherLabel, c);
 		
-		fatherNameField = new JTextField("MonsieurFather", 12);
+		fatherNameField = new JTextField(12);
 		fatherNameField.setEditable(false);
 		c.gridx = 3;
 		c.weightx = 1;
 		c.gridwidth = 7;
 		add(fatherNameField, c);
 		
-		JButton buttonFplus = new JButton("+"); // TODO remplacer par une icone
+		JButton buttonFplus = new JButton(new ImageIcon("resources/Icons/ajouter-16.png"));
 		c.gridx = 10;
 		c.weightx = 0;
 		c.gridwidth = 1;
 		add(buttonFplus, c);
 		
-		JButton buttonFminus = new JButton("-");
+		JButton buttonFminus = new JButton(new ImageIcon("resources/Icons/retirer-16.png"));
 		c.gridx = 11;
 		c.gridwidth = 1;
 		add(buttonFminus, c);
@@ -112,13 +117,13 @@ public class EditPersonDialog extends JDialog implements ActionListener {
 		c.gridwidth = 7;
 		add(motherNameField, c);
 		
-		JButton buttonMplus = new JButton("+"); // TODO remplacer par une icone
+		JButton buttonMplus = new JButton(new ImageIcon("resources/Icons/ajouter-16.png"));
 		c.gridx = 10;
 		c.weightx = 0;
 		c.gridwidth = 1;
 		add(buttonMplus, c);
 		
-		JButton buttonMminus = new JButton("-");
+		JButton buttonMminus = new JButton(new ImageIcon("resources/Icons/retirer-16.png"));
 		c.gridx = 11;
 		c.gridwidth = 1;
 		add(buttonMminus, c);
