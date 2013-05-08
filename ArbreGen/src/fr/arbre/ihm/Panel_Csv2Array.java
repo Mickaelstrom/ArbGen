2833b0ff031f8d64a103e49392b4f5a2bde40660
Mickael <mickaelgoncalves32@gmail.com>
2013-05-08 21:57:58 +0200

    màj par rapport au nouvelles fonctions

--- ArbreGen/src/fr/arbre/ihm/Panel_Csv2Array.java
+++ ArbreGen/src/fr/arbre/ihm/Panel_Csv2Array.java
@@ -1,31 +1,35 @@
 package fr.arbre.ihm;
 
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableColumn;
 
-import fr.arbre.dao.csv.Csv2Array;
+import fr.arbre.dao.csv.CsvPersonDao;
 
 @SuppressWarnings("serial")
 public class Panel_Csv2Array extends JPanel {
 
 	public Panel_Csv2Array() {
 
-		Csv2Array data = new Csv2Array("resources/CSV/gen-01.csv");
-		//JPanel panIcon = new JPanel();
-		String[][] tab = data.toArray();
-		data.PrintArray(tab);
-
+		CsvPersonDao dao = CsvPersonDao.getInstance();
+		dao.load("resources/CSV/gen-dbz.csv");
+		String[][] tab = dao.getTable();
+		DefaultTableModel dtm = new DefaultTableModel(tab, dao.getHeader()) {
+			public boolean isCellEditable(int row, int column) {
+				return false;
+			}
+		};
+		
 		// JTABLE
-		JTable table = new JTable(tab, tab[0]);
-		columnWidth(table, 150, data.getMaxColumn());
+		JTable table = new JTable(dtm);
+		table.doLayout();
+		columnWidth(table, 150, dao.getHeader().length);
 
 		// PANEL
-		JPanel pan = new JPanel();
-
-		this.add(table);
-		this.add(pan);
-
+		JScrollPane scrollPane = new JScrollPane(table);
+		add(scrollPane);
 	}
 
 	public static void columnWidth(JTable table, int width, int maxcolumn) {
