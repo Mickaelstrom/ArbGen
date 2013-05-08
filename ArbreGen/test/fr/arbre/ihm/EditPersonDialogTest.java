b14a79de48339e72db5760ce20e3c88042414e59
Mickael <mickaelgoncalves32@gmail.com>
2013-05-08 21:59:12 +0200

    Amélioration

--- ArbreGen/test/fr/arbre/ihm/EditPersonDialogTest.java
+++ ArbreGen/test/fr/arbre/ihm/EditPersonDialogTest.java
@@ -8,26 +8,16 @@ import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-import javax.swing.UIManager;
 
 import fr.arbre.dao.csv.CsvPersonDao;
 import fr.arbre.dao.csv.PersonIdException;
-import fr.arbre.ihm.EditPersonDialog;
-import fr.arbre.model.Gender;
 import fr.arbre.model.SimplePerson;
 
 /**
- * Test de la fenêtre EditPersonDialog
- * 
- * <p>
- * -> Changement du titre de la fenêtre
- * </p>
- * <p>
- * -> Test création d'une personne
- * </p>
- * <p>
+ * Test de la fenêtre EditPersonDialog <br/>
+ * -> Changement du titre de la fenêtre<br/>
+ * -> Test création d'une personne<br/>
  * -> Test édition d'une personne
- * </p>
  */
 public class EditPersonDialogTest {
 
@@ -35,19 +25,10 @@ public class EditPersonDialogTest {
 
 	public static void main(String[] args) {
 
-		// Changer le style des fenêtres
-		try {
-			UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
-			// UIManager.setLookAndFeel("com.sun.java.swing.plaf.nimbus.NimbusLookAndFeel");
-		} catch (Exception ex) {
-			ex.printStackTrace();
-		}
-
 		final JFrame frame = new JFrame("EditPersonDialog Test");
 		JPanel panel = new JPanel();
 		final JButton b1 = new JButton("Créer personne");
 		final JButton b2 = new JButton("Editer personne");
-		
 
 		CsvPersonDao dao = CsvPersonDao.getInstance();
 		dao.load("resources/CSV/gen-dbz.csv");
@@ -60,7 +41,7 @@ public class EditPersonDialogTest {
 		}
 
 		System.out.println(p2);
-		
+
 		b1.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
@@ -68,8 +49,8 @@ public class EditPersonDialogTest {
 				p1 = EditPersonDialog.showDialog(frame);
 				System.out.println(p1);
 				if (p1 != null)
-					System.out.println("id1= " + p1.getId());
-				System.out.println("id2= " + p2.getId() + " " + p2.toString());
+					System.out.println("n°1 est un nouveau: " + p1.toString());
+				System.out.println("n°2 reste: " + p2.toString());
 			}
 		});
 
@@ -82,8 +63,8 @@ public class EditPersonDialogTest {
 				if (temp != null)
 					p2 = temp;
 				if (p1 != null)
-					System.out.println("id1= " + p1.getId());
-				System.out.println("id2= " + p2.getId() + " " + p2.toString());
+					System.out.println("n°1 reste: " + p1.toString());
+				System.out.println("n°2 est maintenant " + p2.toString());
 			}
 		});
 
