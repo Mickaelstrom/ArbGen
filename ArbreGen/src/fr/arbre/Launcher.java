88a1413c499efd96805eccbe6a07a171c7dc89c1
Mickael <mickaelgoncalves32@gmail.com>
2013-05-08 21:05:54 +0200

    màj

--- ArbreGen/src/fr/arbre/Launcher.java
+++ ArbreGen/src/fr/arbre/Launcher.java
@@ -1,19 +1,26 @@
 package fr.arbre;
 
-import javax.swing.JFrame;
+import javax.swing.UIManager;
+
+import fr.arbre.ihm.Fenetre;
 
 public class Launcher {
 
 	public static void main(String[] args) {
-		/**
-		 * TODO à implémenter correctement au final
-		 */
-		JFrame frame = new JFrame("Test");
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
 
-		frame.pack();
-		frame.setVisible(true);
+		// Change le style des fenêtres
+		// FIXME se mettre d'accord sur le style des fenêtres ??????
+		try {
+			UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
+			// UIManager.setLookAndFeel("com.sun.java.swing.plaf.nimbus.NimbusLookAndFeel");
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+
+		new Fenetre();
+		// mainWindow.setExtendedState(JFrame.MAXIMIZED_BOTH); // FIXME extended or not extended
+		// ????
+
 	}
 
 }
