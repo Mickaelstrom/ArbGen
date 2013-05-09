947534144db381e5a4478ae9cdfd252f71ecb220
Heihong <traingheihong@gmail.com>
2013-05-09 10:45:02 +0200

    Modification de la fenetre en non modale

--- ArbreGen/src/fr/arbre/ihm/Tableau.java
+++ ArbreGen/src/fr/arbre/ihm/Tableau.java
@@ -6,8 +6,6 @@ import javax.swing.JFrame;
 @SuppressWarnings("serial")
 public class Tableau extends JDialog {
 
-	Panel_Csv2Array	tableau	= new Panel_Csv2Array();
-
 	public Tableau(JFrame parent, String title, boolean modal) {
 		// On appelle le construteur de JDialog correspondant
 		super(parent, title, modal);
@@ -17,14 +15,19 @@ public class Tableau extends JDialog {
 		// On spécifie une taille
 		this.setSize(200, 80);
 
+		// La position
+		this.setLocationRelativeTo(null);
+		this.setModal(false);
+
 		this.add(tableau);
 		// this.add(new JLabel("hello"));
 
 		// La boîte ne devra pas être redimensionnable
 		this.setResizable(false);
+
 		// Enfin on l'affiche
 		this.pack();
-		this.setLocationRelativeTo(null);
+
 		this.setVisible(true);
 		// Tout ceci ressemble à ce que nous faisons depuis le début avec notre JFrame.
 	}
