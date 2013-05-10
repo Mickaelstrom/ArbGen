95ec9773657c1ed88a0d3ea1a1160c0f1c1f8089
Mickael <mickaelgoncalves32@gmail.com>
2013-05-10 21:29:20 +0200

    Renommage

---
+++ ArbreGen/src/fr/arbre/ihm/Table_Csv2Array.java
@@ -0,0 +1,55 @@
+package fr.arbre.ihm;
+
+import javax.swing.JLabel;
+import javax.swing.JTable;
+import javax.swing.ListSelectionModel;
+import javax.swing.table.DefaultTableCellRenderer;
+import javax.swing.table.DefaultTableModel;
+
+import fr.arbre.dao.csv.CsvPersonDao;
+
+@SuppressWarnings("serial")
+public class Table_Csv2Array extends JTable {
+
+	public Table_Csv2Array(String filename) {
+		CsvPersonDao dao = CsvPersonDao.getInstance();
+		if (filename != null) {
+			dao.load(filename);
+		}
+
+		String[][] tab = dao.getTable();
+		DefaultTableModel dtm = new DefaultTableModel(tab, dao.getHeader()) {
+			public boolean isCellEditable(int row, int column) {
+				return false;
+			}
+		};
+		setModel(dtm);
+
+		setAutoResizeMode(AUTO_RESIZE_ALL_COLUMNS);
+		getSelectionModel().setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+
+		getColumnModel().getColumn(0).setPreferredWidth(30); // id
+		getColumnModel().getColumn(1).setPreferredWidth(100); // nom
+		getColumnModel().getColumn(2).setPreferredWidth(100); // prenom
+		getColumnModel().getColumn(3).setPreferredWidth(50); // genre
+		getColumnModel().getColumn(4).setPreferredWidth(50); // id père
+		getColumnModel().getColumn(5).setPreferredWidth(50); // id mère
+		getColumnModel().getColumn(6).setPreferredWidth(100); // date de naissance
+		getColumnModel().getColumn(7).setPreferredWidth(120); // nom image
+		getColumnModel().getColumn(8).setPreferredWidth(100); // id des enfants
+
+		DefaultTableCellRenderer centerRenderer = new DefaultTableCellRenderer();
+		centerRenderer.setHorizontalAlignment(JLabel.CENTER);
+		getColumnModel().getColumn(3).setCellRenderer(centerRenderer);
+		getColumnModel().getColumn(4).setCellRenderer(centerRenderer);
+		getColumnModel().getColumn(5).setCellRenderer(centerRenderer);
+
+		// la première est selectionné par défaut
+		setRowSelectionInterval(0, 0);
+	}
+
+	public int getSelectedId() {
+		return Integer.parseInt((String) getValueAt(getSelectedRow(), 0));
+	}
+
+}
