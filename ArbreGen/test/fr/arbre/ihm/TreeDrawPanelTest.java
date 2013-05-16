6ffa78565128896f0f25e7b2a453d2d60ccb33d5
Mickael <mickaelgoncalves32@gmail.com>
2013-05-16 13:37:54 +0200

    gestion concurrence (problème d'affichage)

--- ArbreGen/test/fr/arbre/ihm/TreeDrawPanelTest.java
+++ ArbreGen/test/fr/arbre/ihm/TreeDrawPanelTest.java
@@ -3,25 +3,37 @@ package fr.arbre.ihm;
 import java.awt.BorderLayout;
 
 import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
 
+import fr.arbre.dao.csv.CsvPersonDao;
 import fr.arbre.ihm.TreeDrawPanel;
 
 public class TreeDrawPanelTest {
 
 	public static void main(String[] args) {
-		JFrame frame;
-		TreeDrawPanel panel;
+		// SwingUtilities.invokeLater pour régler les problèmes de timing pour l'affichage d'image
+		// (thread concurrency)
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				JFrame frame;
+				TreeDrawPanel panel;
 
-		panel = new TreeDrawPanel();
+				panel = new TreeDrawPanel();
+				frame = new JFrame("TreeDrawPanel Test");
 
-		frame = new JFrame("TreeDrawPanel Test");
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
-		frame.getContentPane().add(panel, BorderLayout.CENTER);
-		frame.pack();
-		frame.setVisible(true);
+				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+				frame.getContentPane().add(panel, BorderLayout.CENTER);
 
-		panel.drawTest();
+				frame.pack();
+				frame.setVisible(true);
+
+				// panel.drawTest();
+				CsvPersonDao dao = CsvPersonDao.getInstance();
+				dao.load("resources/CSV/gen-dbz.csv");
+				panel.drawTree();
+			}
+		});
 	}
 
 }
